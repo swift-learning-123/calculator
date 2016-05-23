bfd1cd7e71516a46de54c88fb850f4ebbd1f2675
Bidhan Tamuli <bidhan@slideshare.com>
2016-05-22 18:24:11 -0700

    Initial Commit

---
+++ calculato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculato
+//
+//  Created by Bidhan Tamuli on 22/05/16.
+//  Copyright © 2016 bidhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
